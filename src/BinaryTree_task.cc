2e82b75f9b27c0b7bf2ae5e4d29be7adf4ae29a2
Анастасия Мытарева <nastya.mytareva.04@gmail.com>
2024-04-16 15:16:37 +0400

    task

--- src/BinaryTree_task.cc
+++ src/BinaryTree_task.cc
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <vector>
 #include <BinaryTree.h>
 
 using namespace std;
 using namespace nestea;
 
+vector<int> duplicate_elements(vector<int> _vector) {
+	BinaryTree _bintree;
+	vector<int> _duplicate;
+	for (int i = 0; i < _vector.size(); i++)
+		if (!_bintree.insert(_vector[i]))
+			_duplicate.push_back(_vector[i]);
+	return _duplicate;
+}
+
 int main() {
+	vector<int> _vector = { 3, 2, 2, 4 };
+	vector<int> _duplicate = duplicate_elements(_vector);
+	for (int i = 0; i < _duplicate.size(); i++)
+		cout << _duplicate[i] << " ";
 	return 0;
 }
